78044f674649539531fe0880251094dd5224766f
Nathan West <Lucretiel@gmail.com>
2019-04-19 20:42:39 -0700

    Added semi_lazy_format as well as other improvements
    - Added semi_lazy_format
    - lazy_format now creates and returns an inline struct, rather than returning `impl Display`
    - Added substantially more documentation and tests
    - Removed make_lazy_format, LazyFormat

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 #![no_std]
 
-//! [`lazy_format!`] is a macro which lazily formats its arguments. That is, rather
-//! than immediatly formatting them into a
-//! [`String`](https://doc.rust-lang.org/std/string/struct.String.html)
+//! lazy_format is a collection of [`format!`]-style macros which lazily
+//! formatting their arguments its arguments. That is, rather than immediatly
+//! formatting them into a [`String`](https://doc.rust-lang.org/std/string/struct.String.html)
 //! (which is what [`format!`](https://doc.rust-lang.org/std/macro.format.html))
 //! does, it captures its arguments and returns an opaque struct with a
 //! [`Display`](https://doc.rust-lang.org/std/fmt/trait.Display.html)
@@ -12,7 +12,7 @@
 //! ```
 //! use std::fmt::Display;
 //!
-//! use lazy_format::lazy_format;
+//! use lazy_format::prelude::*;
 //!
 //! // NOTE: This is obviously profoundly unsafe and you should never actually
 //! // render HTML without escape guards, code injection prevention, etc.
@@ -20,11 +20,18 @@
 //!     lazy_format!("<{tag}>{content}</{tag}>", tag=tag, content=content)
 //! }
 //!
-//! let result = html_tag("div", html_tag("p", "Hello, World!"));
+//! let result = html_tag("div", html_tag("p", "Hello, World!")).to_string();
 //! assert_eq!(result, "<div><p>Hello, World!</p></div>");
 //! ```
-
-use core::fmt::{self, Debug, Display, Formatter};
+//!
+//! This library contains two lazy formatting macros:
+//!
+//! - [`lazy_format!`], a completely lazy formatter. It captures the expressions
+//! passed as arguments in a closure, and doesn't evaluate them until the
+//! instance is *actually* written to a destination (like a string or file).
+//! This means that
+//! - [`semi_lazy_format`] is partially lazy. It fully evaluates all of its
+//! arguments when it is invoked, but it stores them
 
 /// Lazily format something
 ///
@@ -33,11 +40,21 @@ use core::fmt::{self, Debug, Display, Formatter};
 /// instead of formatting its arguments to a string, it captures them in an opaque
 /// struct, which can be formatted later. This allows you to build up formatting
 /// operations without any intermediary allocations or extra formatting calls.
-/// See the [module-level documentation](/lazy_format) for details.
 ///
-/// The return type of this macro is `impl Display + Debug`. When Rust's
-/// `impl trait` feature is more powerful, this will also include traits that
-/// are conditional on the caputred variables, like `Clone`.
+/// The return value of this macro is left deliberately unspecified and
+/// undocumented. The most important this about it is its `Display`
+/// implementation, which executes the deferred formatting operation. It
+/// also provides a `Debug` implementation, which simply prints the
+/// `lazy_format!(...)` call without evaluating any of its arguments, as well
+/// as `Clone` and `Copy` if those traits are available in the captured values.
+///
+/// Note that this macro is completely lazy; it captures the expressions to
+/// be formatted in the struct and doesn't evaluate them until the struct is
+/// actually written to a `String` or `File` or or other writable destination.
+/// This means that the argument expression will be evaluated *every* time the
+/// instance is written, which may not be what you want. See [`semi_lazy_format`]
+/// for a macro which eagerly evaluates its arguments but lazily does the final
+/// formatting.
 ///
 /// # Example:
 ///
@@ -60,153 +77,175 @@ use core::fmt::{self, Debug, Display, Formatter};
 /// let result = hello_world();
 ///
 /// // get_hello and get_world aren't called until the object is
-/// // formatted into a String
-/// let result_str = result;
+/// // formatted into a String.
+/// let result_str = result.to_string();
 /// assert_eq!(result_str, "Hello, World!");
 /// ```
 #[macro_export]
 macro_rules! lazy_format {
-    ($pattern:literal) => {
-        $crate::make_lazy_format(#[inline] move |f: &mut ::core::fmt::Formatter| -> ::core::fmt::Result {
-            write!(f, $pattern)
-        }) };
-    ($pattern:literal, $($args:tt)*) => {
-        $crate::make_lazy_format(#[inline] move |f: &mut ::core::fmt::Formatter| -> ::core::fmt::Result {
-            write!(f, $pattern, $($args)*)
+    ($pattern:literal $($args:tt)*) => {{
+        #[derive(Clone, Copy)]
+        struct LazyFormat<F: Fn(&mut ::core::fmt::Formatter) -> ::core::fmt::Result>(F);
+
+        // TODO: customize Debug impl for semi_lazy_format to include value
+        impl<F: Fn(&mut ::core::fmt::Formatter) -> ::core::fmt::Result> ::core::fmt::Debug for LazyFormat<F> {
+            #[inline]
+            fn fmt(&self, f: &mut ::core::fmt::Formatter) -> ::core::fmt::Result {
+                f.write_str(concat!("lazy_format!(", stringify!( $pattern $($args)* ), ")"))
+            }
+        }
+
+        impl<F: Fn(&mut ::core::fmt::Formatter) -> ::core::fmt::Result> ::core::fmt::Display for LazyFormat<F> {
+            #[inline]
+            fn fmt(&self, f: &mut ::core::fmt::Formatter) -> ::core::fmt::Result {
+                (self.0)(f)
+            }
+        }
+
+        LazyFormat(#[inline] move |f: &mut ::core::fmt::Formatter| -> ::core::fmt::Result {
+            write!(f, $pattern $($args)*)
         })
-    };
+    }};
 }
 
+/// Lazily format something, but eagerly evaluate the arguments ahead of time.
+///
+/// This macro is essentially the same as
+/// [`format!`](https://doc.rust-lang.org/std/macro.format.html), except that
+/// instead of formatting its arguments to a string, it evaluates them and
+/// captures them in an opaque struct, which can be formatted later. This allows
+/// you to build up formatting operations without any extra allocations or
+/// formatting calls.
+///
+/// The return value of this macro is left deliberately unspecified and
+/// undocumented. The most important this about it is its `Display`
+/// implementation, which executes the deferred formatting operation. It
+/// also provides `Clone` and `Copy` if those traits are implementated in all
+/// of the evaluated formatting arguments.
+///
+/// Unlike [`lazy_format`], this macro is partially lazy, in that it evaluates
+/// its argument expressions and stores the values to be formatted later. This
+/// is often more convenient, especially if the formatted values are simple
+/// data types like integers and `&str`. This can also make using the value
+/// easier, as its less likely to capture locally scoped variable or references
+/// and therefore have lifetime issues.
+///
+///
+/// ```
+/// use std::fmt::Display;
+/// use std::mem::{size_of_val, size_of};
+/// use lazy_format::semi_lazy_format;
+///
+///
+/// fn get_value() -> usize {
+///     1024
+/// }
+///
+/// fn get_formatted() -> impl Display {
+///     semi_lazy_format!("value: {v}, again: {v}", v = get_value())
+/// }
+///
+/// let result = get_formatted();
+///
+/// // At this point, get_value was called, and `result` stores just the
+/// // usize return value. No allocations have been performed yet.
+/// let result_str = result.to_string();
+/// assert_eq!(result_str, "value: 1024, again: 1024");
+/// assert_eq!(size_of_val(&result), size_of::<usize>());
+/// ```
 #[macro_export]
 macro_rules! semi_lazy_format {
-
-    ($pattern:literal, $($args:tt)*) => {
-        $crate::semi_lazy_format_impl!($pattern, $($args)*)
+    // TODO: Debug implementation with values
+    ($pattern:literal $($args:tt)*) => {
+        $crate::semi_lazy_format_impl!($pattern $($args)*)
     };
 }
 
+/// Implementation macro semi_lazy_format
 #[doc(hidden)]
 #[macro_export]
 macro_rules! semi_lazy_format_impl {
-    (
-        $({ $evaluated_value:ident $($fmt_name:ident)?})*
-    @ $pattern:literal) => {
+    ($({ $evaluated_value:ident $($fmt_name:ident)?})* $pattern:literal) => {
         $crate::lazy_format!($pattern $(, $($fmt_name =)? $evaluated_value)*)
     };
 
-    (
-        $({ $evaluated_value:ident $($fmt_name:ident)?})*
-    @ $pattern:literal,) => {
-        $crate::lazy_format!($pattern $(, $($fmt_name =)? $evaluated_value)*)
+    ($({ $evaluated_value:ident $($fmt_name:ident)?})* $pattern:literal,) => {
+        $crate::semi_lazy_format_impl!($({ $evaluated_value $($fmt_name)? })* $pattern)
     };
 
-    (
-        $({ $evaluated_value:ident $($fmt_name:ident)?})*
-    @ $pattern:literal, $name:ident = $value:expr) => {{
+    // Important: the name = value variants must come first, because "name = value"
+    // is a valid rust expr
+    ($({ $evaluated_value:ident $($fmt_name:ident)?})* $pattern:literal, $name:ident = $value:expr) => {
+        $crate::semi_lazy_format_impl!($({ $evaluated_value $($fmt_name)? })* $pattern, $name = $value,)
+    };
+
+    ($({ $evaluated_value:ident $($fmt_name:ident)?})* $pattern:literal, $name:ident = $value:expr, $($tail:tt)*) => {{
         let value = $value;
         $crate::semi_lazy_format_impl!(
             $({ $evaluated_value $($fmt_name)? })*
             { value $name }
-        @ $pattern)
+        $pattern, $($tail)*)
     }};
 
-    (
-        $({ $evaluated_value:ident $($fmt_name:ident)?})*
-    @ $pattern:literal, $value:expr) => {{
+    ($({ $evaluated_value:ident $($fmt_name:ident)?})* $pattern:literal, $value:expr) => {
+        $crate::semi_lazy_format_impl!($({ $evaluated_value $($fmt_name)? })* $pattern, $value, )
+    };
+
+    ($({ $evaluated_value:ident $($fmt_name:ident)?})* $pattern:literal, $value:expr, $($tail:tt)*) => {{
         let value = $value;
         $crate::semi_lazy_format_impl!(
             $({ $evaluated_value $($fmt_name)? })*
             { value }
-        @ $pattern)
+        $pattern, $($tail)*)
     }};
-
-/*
-    ({
-        $({ $evaluated_value:ident $($fmt_name:ident)?})*
-    } @ $pattern:literal, $name:ident = $value:expr, $($tail:tt)*) => {{
-        let value = $value;
-        $crate::semi_lazy_format_impl!({
-            $({ $evaluated_value $($fmt_name)? })*
-            { value $name }
-        } @ $pattern, $($tail)*)
-    }};
-
-    ({
-        $({ $evaluated_value:ident $($fmt_name:ident)?})*
-    } @ $pattern:literal, $value:expr, $($tail:tt)*) => {{
-        let value = $value;
-        $crate::semi_lazy_format_impl!({
-            $({ $evaluated_value $($fmt_name)? })*
-            { value }
-        } @ $pattern, $($tail)*)
-    }};
-    */
-}
-
-#[doc(hidden)]
-#[inline(always)]
-pub fn make_lazy_format<F: Fn(&mut Formatter) -> fmt::Result>(f: F) -> impl Display + Debug {
-    LazyFormat(f)
-}
-
-#[derive(Clone, Copy, PartialEq, Eq)]
-struct LazyFormat<F: Fn(&mut Formatter) -> fmt::Result>(F);
-
-impl<F: Fn(&mut Formatter) -> fmt::Result> Debug for LazyFormat<F> {
-    #[inline]
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        f.write_str("LazyFormat(<closure>)")
-    }
-}
-
-impl<F: Fn(&mut Formatter) -> fmt::Result> Display for LazyFormat<F> {
-    #[inline]
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        (self.0)(f)
-    }
 }
 
 pub mod prelude {
-    pub use crate::lazy_format;
+    pub use crate::{lazy_format, semi_lazy_format};
 }
 
+/// The syntax of semi_lazy_format is fairly complicated; these tests are
+/// provided to ensure there are no failures. Putting them here (rather than
+/// in /tests) allows us to get better compiler diagnostics.
 #[cfg(test)]
 mod semi_lazy_format_syntax_tests {
-    use crate::semi_lazy_format;
     #[test]
     fn test_no_args() {
-        let result = semi_lazy_format!("Hello, World!");
+        let _result = semi_lazy_format!("Hello, World!");
     }
 
     #[test]
     fn test_trailing_comma() {
-        let result = semi_lazy_format!("Hello, World!",);
+        let _result = semi_lazy_format!("Hello, World!",);
     }
 
     #[test]
     fn test_one_arg() {
-        let result = semi_lazy_format!("{}!", "Hello, World");
+        let _result = semi_lazy_format!("{}!", "Hello, World");
+    }
+
+    #[test]
+    fn test_two_args() {
+        let _result = semi_lazy_format!("{}, {}!", "Hello", "World");
     }
 
     #[test]
     fn test_one_named_arg() {
-        let result = semi_lazy_format!("{text}!", text = "Hello, World");
-    }
-
-    /*
-    #[test]
-    fn test_two_args() {
-        let result = semi_lazy_format!("{}, {}!", "Hello", "World");
+        let _result = semi_lazy_format!("{text}!", text="Hello, World");
     }
 
     #[test]
     fn test_two_named_args() {
-        let result = semi_lazy_format!("{h}, {w}!", h="Hello", w="World");
+        let _result = semi_lazy_format!("{h}, {w}!", h="Hello", w="World");
     }
 
     #[test]
     fn test_mixed_args() {
-        let result = semi_lazy_format!("{}, {w}!", "Hello", w="World");
+        let _result = semi_lazy_format!("{}, {w}!", "Hello", w="World");
     }
-    */
+
+    #[test]
+    fn test_many_args() {
+        let _result = semi_lazy_format!("{} {} {} {a} {b} {} {b} {a} {}", 1, 2, 3, 4, 5, a=10, b=20);
+    }
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,12 +1,36 @@
+#![cfg(test)]
+
+use std::cell::Cell;
+
 // We put all the tests in a separate test crate, to ensure that macro imports
 // work correctly, and also to give us access to std
 
+#[derive(Debug, Clone, Default, Eq, PartialEq)]
+struct ValueEmitter {
+    count: Cell<usize>,
+}
+
+impl ValueEmitter {
+    fn new() -> Self {
+        ValueEmitter {count: Cell::new(0)}
+    }
+
+    fn next(&self) -> String {
+        self.count.set(self.count.get() + 1);
+        return format!("{}", self.count.get())
+    }
+
+    fn count(&self) -> usize {
+        self.count.get()
+    }
+}
+
 mod lazy_format {
-    use std::cell::Cell;
     use std::fmt::Display;
     use std::fmt::Write;
 
     use lazy_format::lazy_format;
+    use crate::ValueEmitter;
 
     #[test]
     fn basic_format() {
@@ -23,45 +47,45 @@ mod lazy_format {
 
     #[test]
     fn ensure_lazy() {
-        let call_count = Cell::new(0);
+        let emitter = &ValueEmitter::new();
 
-        let get_value = || {
-            call_count.set(call_count.get() + 1);
-            return 10;
-        };
 
-        let lazily_formatted = lazy_format!("{}, {} ({})", "Hello", "World", get_value());
-        let lazy2 = lazy_format!("{} {} {}", get_value(), get_value(), get_value());
+        let lazily_formatted = lazy_format!("{}, {} ({})", "Hello", "World", emitter.next());
+        let lazy2 = lazy_format!("{} {} {}", emitter.next(), emitter.next(), emitter.next());
 
         // At this point, the cell should not have been incremented at all
-        assert_eq!(call_count, Cell::new(0));
+        assert_eq!(emitter.count(), 0);
 
+        //
         let mut dest = String::new();
-
         write!(&mut dest, "{}\n{}", lazily_formatted, lazy2).unwrap();
-        assert_eq!(dest, "Hello, World (10)\n10 10 10");
-        assert_eq!(call_count, Cell::new(4));
+        assert_eq!(dest, "Hello, World (1)\n2 3 4");
+        assert_eq!(emitter.count(), 4);
+
+        // Because the formatting is lazy, emitter.next() will continue to be called
+        // every time the lazy_format values are formatted
+        dest.clear();
+        write!(&mut dest, "{}\n{}", lazily_formatted, lazy2).unwrap();
+        assert_eq!(dest, "Hello, World (5)\n6 7 8");
+        assert_eq!(emitter.count(), 8);
     }
 
     #[test]
     fn test_recursive() {
-        let call_count = Cell::new(0);
+        let emitter = &ValueEmitter::new();
 
-        let get_value = || {
-            call_count.set(call_count.get() + 1);
-            return call_count.get();
-        };
-
-        let lazy1 = lazy_format!("{}, {}", get_value(), get_value());
+        let lazy1 = lazy_format!("{}, {}", emitter.next(), emitter.next());
         let lazy2 = lazy_format!("({lazy}), ({lazy})", lazy = lazy1);
         let lazy3 = lazy_format!("({lazy}), ({lazy})", lazy = lazy2);
 
-        assert_eq!(call_count, Cell::new(0));
+        assert_eq!(emitter.count(), 0);
 
-        let result = lazy3.to_string();
 
-        assert_eq!(result, "((1, 2), (3, 4)), ((5, 6), (7, 8))");
-        assert_eq!(call_count, Cell::new(8));
+        assert_eq!(lazy3.to_string(), "((1, 2), (3, 4)), ((5, 6), (7, 8))");
+        assert_eq!(emitter.count(), 8);
+
+        assert_eq!(lazy3.to_string(), "((9, 10), (11, 12)), ((13, 14), (15, 16))");
+        assert_eq!(emitter.count(), 16);
     }
 
     #[test]
@@ -81,12 +105,85 @@ mod lazy_format {
     fn test_result_value_with_lifetime() {
         // This function tests that the return value of lazy_format (and specifically
         // of make_lazy_format) fulfills the lifetime bound
-        fn double_str<'a>(s: &'a str) -> impl Display + 'a {
+        fn double_str<'a>(s: &'a str) -> impl Display + 'a{
             lazy_format!("{}, {}", s, s)
         }
 
         let content = "Hello".to_string();
         let result = double_str(content.as_str()).to_string();
         assert_eq!(result, "Hello, Hello");
+    }
+}
+
+mod semi_lazy_format {
+    use std::fmt::Display;
+
+    use lazy_format::semi_lazy_format;
+
+    use crate::ValueEmitter;
+
+    #[test]
+    fn no_args() {
+        let result = semi_lazy_format!("Hello, World!").to_string();
+        assert_eq!(result, "Hello, World!");
+    }
+
+    #[test]
+    fn one_arg(){
+        let result = semi_lazy_format!("{}!", "Hello, World").to_string();
+        assert_eq!(result, "Hello, World!");
+    }
+
+    #[test]
+    fn two_args() {
+        let result = semi_lazy_format!("{}, {}!", "Hello", "World").to_string();
+        assert_eq!(result, "Hello, World!");
+    }
+
+    #[test]
+    fn named_args() {
+        let result = semi_lazy_format!("{h}, {w}!", h="Hello", w="World").to_string();
+        assert_eq!(result, "Hello, World!");
+    }
+
+    #[test]
+    fn reverse_named_args() {
+        let result = semi_lazy_format!("{h}, {w}!", w="World", h="Hello").to_string();
+        assert_eq!(result, "Hello, World!");
+    }
+
+    #[test]
+    fn test_evaluate_once() {
+        let emitter = ValueEmitter::new();
+
+        let formatted = semi_lazy_format!("{} {b} {a} {b} {}",
+            emitter.next(),
+            emitter.next(),
+            a=emitter.next(),
+            b=emitter.next(),
+        );
+
+        // At this point, the emitted should have been advanced
+        assert_eq!(emitter.count(), 4);
+
+        let result1 = formatted.to_string();
+        assert_eq!(result1, "1 4 3 4 2");
+
+        let result2 = formatted.to_string();
+        assert_eq!(result2, "1 4 3 4 2");
+
+        // The emitter should not have been touched
+        assert_eq!(emitter.count(), 4);
+    }
+
+    /// Test that lazy_format drops lifetimes by evaluating the arguments
+    /// immediately.
+    #[test]
+    fn test_dropped_lifetime() {
+        fn get_value<'a>(value: &'a str) ->  impl Display {
+            semi_lazy_format!("{} {a}", String::from(value), a = String::from(value))
+        }
+
+        assert_eq!(get_value("HELLO").to_string(), "HELLO HELLO");
     }
 }
